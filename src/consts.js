8060da7dbff11af841a1927c0bf8653e7a21f110
Tyler <44188028+twit96@users.noreply.github.com>
2023-10-26 20:59:21 -0500

    Add ReplyToPageBtn Component, PageUtils Component
    PageUtils wraps ReplyToPageBtn and ScrollTopBtn

--- src/consts.js
+++ src/consts.js
@@ -3,3 +3,4 @@ export const SITE_TITLE = 'Tyler Wittig | Full-Stack Developer';
 export const SITE_DESCRIPTION = 'Full-Stack Web Developer in South Texas, specializing in custom websites and website redesigns for small businesses.';
 export const SITE_IMAGE = '/img/og-image.png';
 export const SITE_AUTHOR = 'Tyler Wittig';
+export const CONTACT_EMAIL = 'tylerwittig.work@gmail.com';
