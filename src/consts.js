18adeedb7d3208b7960941df74b8b906b178cf1c
Tyler <44188028+twit96@users.noreply.github.com>
2023-11-07 10:52:54 -0600

    Replace SocialLinks with new ContactLinks Component
    Stored contact information in "src/consts.js".
    
    Created new ContactLinks component, which stores an object that imports the contact information from consts.js and contains link data for it. The component shows all contact information by default, but can have an array of specific links to show passed in its props instead.
    
    Replaced SocialLinks imports with ContactLinks in Homepage and Footer. Deleted SocialLinks component.
    
    Replaced CONTACT_EMAIL import with CONTACT_INFO in ReplyToPageLink component. Removed CONTACT_EMAIL constant from consts.js.

--- src/consts.js
+++ src/consts.js
@@ -3,4 +3,13 @@ export const SITE_TITLE = 'Tyler Wittig | Full-Stack Developer';
 export const SITE_DESCRIPTION = 'Full-Stack Web Developer in South Texas, specializing in custom websites and website redesigns for small businesses.';
 export const SITE_IMAGE = '/img/og-image.png';
 export const SITE_AUTHOR = 'Tyler Wittig';
-export const CONTACT_EMAIL = 'tylerwittig.work@gmail.com';
+
+export const CONTACT_INFO = {
+  'Codepen':  'https://codepen.io/twit96',
+  'Email':    'tylerwittig.work@gmail.com',
+  'GitHub':   'https://github.com/twit96',
+  'LinkedIn': 'https://www.linkedin.com/in/tylerwittig/',
+  'Resume':   '/resume-tylerwittig-20230424.pdf',
+  'RSS':      '/rss.xml',
+  'Twitter':  'https://twitter.com/tyler_wittig'
+};
