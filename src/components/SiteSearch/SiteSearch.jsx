847ef4b6f8bb0b90a8fdd01bad47ecf7e49f7439
Tyler <44188028+twit96@users.noreply.github.com>
2023-11-08 21:44:55 -0600

    Rename "heading--reduce" to "heading--reduced"
    For consistency with the "heading--diminished" CSS class, change the "heading--reduce" CSS class name to "heading--reduced".

--- src/components/SiteSearch/SiteSearch.jsx
+++ src/components/SiteSearch/SiteSearch.jsx
@@ -74,7 +74,7 @@ function Search({ searchList }) {
                       </b>
                     </div>
 
-                    <h2 className={`heading--reduce ${styles.resultTitle}`}>
+                    <h2 className={`heading--reduced ${styles.resultTitle}`}>
                       <a href={`/${result.item.data.category}/${result.item.slug}`}>
                         {result.item.data.title}
                       </a>
